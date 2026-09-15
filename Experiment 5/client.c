c8baf7c5d0b6b01be6889507b9ce67376bc4e153
ABHISHEK B <abhishekb05cec@gmail.com>
2026-09-15 23:53:14 +0530

    Create client.c

---
+++ Experiment 5/client.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+
+#define PORT 8080
+#define BUF_SIZE 1024
+
+int main() {
+    int sock;
+    struct sockaddr_in server_address;
+    char buffer[BUF_SIZE];
+
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(PORT);
+    inet_pton(AF_INET, "127.0.0.1", &server_address.sin_addr);
+
+    if (connect(sock, (struct sockaddr *)&server_address, sizeof(server_address)) < 0) {
+        perror("connect failed");
+        exit(1);
+    }
+
+    printf("Connected to server. Type 'exit' to quit.\n");
+
+    while (1) {
+        printf("You: ");
+        fgets(buffer, BUF_SIZE, stdin);
+        buffer[strcspn(buffer, "\n")] = 0;  // remove trailing newline
+
+        send(sock, buffer, strlen(buffer), 0);
+
+        if (strcmp(buffer, "exit") == 0)
+            break;
+
+        memset(buffer, 0, BUF_SIZE);
+        recv(sock, buffer, BUF_SIZE, 0);
+        printf("Server echoed: %s\n", buffer);
+    }
+
+    close(sock);
+    return 0;
+}
