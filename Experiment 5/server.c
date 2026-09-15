596dc9a1245e2b2e5c8492ba50ee8febdecac7a7
ABHISHEK B <abhishekb05cec@gmail.com>
2026-09-16 00:41:56 +0530

    Refactor client handling into main loop

--- Experiment 5/server.c
+++ Experiment 5/server.c
@@ -9,32 +9,11 @@
 #define PORT 8080
 #define BUF_SIZE 1024
 
-void handle_client(int client_socket) {
-    char buffer[BUF_SIZE];
-    int n;
-
-    while (1) {
-        memset(buffer, 0, BUF_SIZE);
-        n = recv(client_socket, buffer, BUF_SIZE, 0);
-        if (n <= 0) {
-            printf("Client disconnected.\n");
-            break;
-        }
-        printf("Received: %s\n", buffer);
-
-        // Echo back to client
-        send(client_socket, buffer, strlen(buffer), 0);
-
-        if (strcmp(buffer, "exit") == 0)
-            break;
-    }
-    close(client_socket);
-}
-
 int main() {
     int server_socket, client_socket;
     struct sockaddr_in server_address, client_address;
     socklen_t client_len = sizeof(client_address);
+    char buffer[BUF_SIZE];
 
     server_socket = socket(AF_INET, SOCK_STREAM, 0);
 
@@ -45,23 +24,38 @@ int main() {
     bind(server_socket, (struct sockaddr *)&server_address, sizeof(server_address));
     listen(server_socket, 5);
 
-    printf("Server listening on port %d...\n", PORT);
+    printf("Server waiting for clients on port %d...\n", PORT);
 
     while (1) {
         client_socket = accept(server_socket, (struct sockaddr *)&client_address, &client_len);
-        printf("New client connected: %s\n", inet_ntoa(client_address.sin_addr));
+        printf("Client connected: %s\n", inet_ntoa(client_address.sin_addr));
 
         pid_t pid = fork();
 
         if (pid == 0) {
-            // Child process handles this client
-            close(server_socket);       // child doesn't need listening socket
-            handle_client(client_socket);
+            close(server_socket);
+
+            while (1) {
+                memset(buffer, 0, BUF_SIZE);
+
+                int n = recv(client_socket, buffer, BUF_SIZE, 0);
+                if (n <= 0) {
+                    printf("Client disconnected.\n");
+                    break;
+                }
+
+                printf("Received: %s\n", buffer);
+                send(client_socket, buffer, strlen(buffer), 0);
+
+                if (strcmp(buffer, "exit") == 0)
+                    break;
+            }
+
+            close(client_socket);
             exit(0);
         } else {
-            // Parent process
-            close(client_socket);       // parent doesn't need this client's socket
-            waitpid(-1, NULL, WNOHANG); // clean up finished children (non-blocking)
+            close(client_socket);
+            waitpid(-1, NULL, WNOHANG);
         }
     }
 
