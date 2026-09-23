3fc6ad45cbc70effcda070fad92bf5b49be9919b
ABHISHEK B <abhishekb05cec@gmail.com>
2026-09-23 15:27:43 +0530

    Refactor client.c for improved readability

--- Experiment 7/client.c
+++ Experiment 7/client.c
@@ -13,53 +13,38 @@ int main() {
     struct sockaddr_in server_address;
     char filename[256];
     char response[BUF_SIZE];
-
     sock = socket(AF_INET, SOCK_STREAM, 0);
-
     if (sock < 0) {
         perror("Socket creation failed");
         exit(1);
     }
-
     server_address.sin_family = AF_INET;
     server_address.sin_port = htons(PORT);
     inet_pton(AF_INET, "127.0.0.1", &server_address.sin_addr);
-
     if (connect(sock, (struct sockaddr *)&server_address,
                 sizeof(server_address)) < 0) {
         perror("Connection failed");
         close(sock);
         exit(1);
     }
-
     while (1) {
         printf("\nEnter filename required (or 'quit' to exit): ");
-
         fgets(filename, sizeof(filename), stdin);
         filename[strcspn(filename, "\n")] = '\0';
-
         if (strcmp(filename, "quit") == 0) {
             printf("Closing connection...\n");
             break;
         }
-
         send(sock, filename, strlen(filename), 0);
-
         memset(response, 0, BUF_SIZE);
-
         int bytes_received = recv(sock, response, BUF_SIZE - 1, 0);
-
         if (bytes_received <= 0) {
             printf("Server disconnected.\n");
             break;
         }
-
         response[bytes_received] = '\0';
-
         printf("Server response: %s\n", response);
     }
-
     close(sock);
-
     return 0;
 }
